214aa13eeb40e42494e9093fbed30ad9bd06e875
Aduda-Boaz <60380365+Aduda-Boaz@users.noreply.github.com>
2020-03-03 15:38:12 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      { title, body }
+    ])
+    setTitle('');
+    setBody('')
+  }
+
+  const removeNote = (title) =>{
+    setNotes(notes.filter((note) => note.title !==title))
+  }
+
+  useEffect(() => {
+    const notesData = JSON.parse(localStorage.getItem('notes'));
+    if (notesData) {
+      setNotes(notesData)
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  return (
+        <div>
+          <h1>Notes</h1>
+      {notes.map((note) => {
+        return (
+          <div>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => removeNote(note.title)}>x</button>
+          </div>
+        )
+      })}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button>Add Note</button>
+      </form>{" "}
+    </div>
+  )
+}
+
+export default App;
